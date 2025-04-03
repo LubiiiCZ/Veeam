184e64e6f08d5ce8a09514cd404171a9c30c9c10
Lubos Dohnal <e-lubos.dohnal@embedit.com>
2025-04-03 10:06:54 +0200

    Getting the arguments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Veeam;
+
+class Program
+{
+    private static string _sourcePath = string.Empty;
+    private static string _replicaPath = string.Empty;
+    private static string _logFilePath = string.Empty;
+    private static int _syncInterval = 0;
+
+    public static void Main(string[] args)
+    {
+        if (args.Length != 4)
+        {
+            Console.WriteLine("Missing program arguments, follow: Program.exe sourcePath replicaPath syncInterval(sec) logPath");
+            return;
+        }
+
+        _sourcePath = args[0];
+        _replicaPath = args[1];
+        if (!int.TryParse(args[2], out _syncInterval) || _syncInterval < 1)
+        {
+            Console.WriteLine("Invalid interval. Must be a positive integer value (in seconds).");
+            return;
+        }
+        _logFilePath = args[3];
+    }
+}
