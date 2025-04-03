0a206ad618f825aff05bbeb2b29e65b09ce8211e
Lubos Dohnal <e-lubos.dohnal@embedit.com>
2025-04-03 16:06:09 +0200

    Timer added + refactoring into DirectorySyncer class

---
+++ DirectorySyncer.cs
@@ -0,0 +1,174 @@
+using System.Security.Cryptography;
+
+namespace Veeam;
+
+public class DirectorySyncer
+{
+    private readonly MD5 _md5 = MD5.Create();
+    private readonly string _sourcePath;
+    private readonly string _replicaPath;
+    private readonly string _logPath;
+    private readonly int _syncInterval;
+    private Timer? _timer;
+
+    public DirectorySyncer(string sourcePath, string replicaPath, int syncInterval, string logPath)
+    {
+        _sourcePath = sourcePath;
+        _replicaPath = replicaPath;
+        _syncInterval = syncInterval;
+        _logPath = logPath;
+
+        VerifyDirectories();
+    }
+
+    private void VerifyDirectories()
+    {
+        if (!Directory.Exists(_sourcePath))
+        {
+            Console.WriteLine($"Source path '{_sourcePath}' does not exist.");
+            Environment.Exit(1);
+        }
+
+        TryCreateDirectory(_replicaPath, true);
+        TryCreateDirectory(_logPath, true);
+    }
+
+    public void StartSync()
+    {
+        _timer = new Timer(SyncDirectories, null, 0, _syncInterval * 1000);
+    }
+
+    private void SyncDirectories(object? state)
+    {
+        try
+        {
+            Console.WriteLine($"Syncing... {DateTime.Now}");
+            CreateDirectories();
+            DeleteDirectories();
+            CopyFiles();
+            DeleteFiles();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    private void CreateDirectories()
+    {
+        var sourceDirs = Directory.GetDirectories(_sourcePath, "*", SearchOption.AllDirectories);
+
+        foreach (var dir in sourceDirs)
+        {
+            var replicaDir = dir.Replace(_sourcePath, _replicaPath);
+            TryCreateDirectory(replicaDir);
+        }
+    }
+
+    private void DeleteDirectories()
+    {
+        var replicaDirs = Directory.GetDirectories(_replicaPath, "*", SearchOption.AllDirectories);
+
+        foreach (var dir in replicaDirs)
+        {
+            if (!Directory.Exists(dir)) continue; //Could have been deleted as part of the sync process
+
+            var sourceDir = dir.Replace(_replicaPath, _sourcePath);
+            if (Directory.Exists(sourceDir)) continue; //Source directory exists, so we don't need to delete the replica
+
+            TryDeleteWholeDirectory(dir);
+        }
+    }
+
+    private void CopyFiles()
+    {
+        var sourceFiles = Directory.GetFiles(_sourcePath, "*", SearchOption.AllDirectories);
+
+        foreach (var file in sourceFiles)
+        {
+            var replicaFile = file.Replace(_sourcePath, _replicaPath);
+            TryCopyFile(file, replicaFile);
+        }
+    }
+
+    private void DeleteFiles()
+    {
+        var replicaFiles = Directory.GetFiles(_replicaPath, "*", SearchOption.AllDirectories);
+
+        foreach (var file in replicaFiles)
+        {
+            var sourceFile = file.Replace(_replicaPath, _sourcePath);
+
+            if (File.Exists(sourceFile)) continue; //Source file exists and the content is the same, so we don't need to delete the replica
+
+            TryDeleteFile(file);
+        }
+    }
+
+    private void TryCreateDirectory(string path, bool exitOnError = false)
+    {
+        try
+        {
+            Directory.CreateDirectory(path);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating directory '{path}': {ex.Message}");
+            if (exitOnError) Environment.Exit(1);
+        }
+    }
+
+    private void TryDeleteWholeDirectory(string path)
+    {
+        try
+        {
+            Directory.Delete(path, true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while deleting directory '{path}': {ex.Message}");
+        }
+    }
+
+    private bool FilesAreEqual(string file1, string file2)
+    {
+        try
+        {
+            using var stream1 = File.OpenRead(file1);
+            using var stream2 = File.OpenRead(file2);
+            return _md5.ComputeHash(stream1).SequenceEqual(_md5.ComputeHash(stream2));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while comparing files '{file1}' and '{file2}': {ex.Message}");
+            return false;
+        }
+    }
+
+    private void TryCopyFile(string sourceFile, string replicaFile)
+    {
+        if (!File.Exists(replicaFile) || !FilesAreEqual(sourceFile, replicaFile))
+        {
+            try
+            {
+                File.Copy(sourceFile, replicaFile, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while copying file from '{sourceFile}' to '{replicaFile}': {ex.Message}");
+            }
+        }
+    }
+
+    private void TryDeleteFile(string path)
+    {
+        try
+        {
+            File.Delete(path);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while deleting file '{path}': {ex.Message}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,22 +2,9 @@
 
 class Program
 {
-    private static string _sourcePath = string.Empty;
-    private static string _replicaPath = string.Empty;
-    private static string _logPath = string.Empty;
-    private static int _syncInterval = 0;
+    private static DirectorySyncer? _directorySyncer;
 
     public static void Main(string[] args)
-    {
-        VerifyArguments(args);
-        VerifyDirectories();
-        CreateDirectories(_sourcePath, _replicaPath);
-        DeleteDirectories(_sourcePath, _replicaPath);
-        CopyFiles(_sourcePath, _replicaPath);
-        DeleteFiles(_sourcePath, _replicaPath);
-    }
-
-    private static void VerifyArguments(string[] args)
     {
         if (args.Length != 4)
         {
@@ -25,76 +12,19 @@ class Program
             return;
         }
 
-        _sourcePath = args[0];
-        _replicaPath = args[1];
-        if (!int.TryParse(args[2], out _syncInterval) || _syncInterval < 1)
+        var sourcePath = args[0];
+        var replicaPath = args[1];
+        if (!int.TryParse(args[2], out var syncInterval) || syncInterval < 1)
         {
             Console.WriteLine("Invalid interval. Must be a positive integer value (in seconds).");
             return;
         }
-        _logPath = args[3];
-    }
+        var logPath = args[3];
 
-    private static void VerifyDirectories()
-    {
-        if (!Directory.Exists(_sourcePath))
-        {
-            Console.WriteLine($"Source path '{_sourcePath}' does not exist.");
-            return;
-        }
+        _directorySyncer = new DirectorySyncer(sourcePath, replicaPath, syncInterval, logPath);
+        _directorySyncer.StartSync();
 
-        Util.TryCreateDirectory(_replicaPath);
-        Util.TryCreateDirectory(_logPath);
-    }
-
-    private static void CreateDirectories(string source, string replica)
-    {
-        var sourceDirs = Directory.GetDirectories(source, "*", SearchOption.AllDirectories);
-
-        foreach (var dir in sourceDirs)
-        {
-            var replicaDir = dir.Replace(source, replica);
-            Util.TryCreateDirectory(replicaDir);
-        }
-    }
-
-    private static void DeleteDirectories(string source, string replica)
-    {
-        var replicaDirs = Directory.GetDirectories(replica, "*", SearchOption.AllDirectories);
-
-        foreach (var dir in replicaDirs)
-        {
-            if (!Directory.Exists(dir)) continue; //Could have been deleted as part of the sync process
-
-            var sourceDir = dir.Replace(replica, source);
-            if (Directory.Exists(sourceDir)) continue; //Source directory exists, so we don't need to delete the replica
-
-            Util.TryDeleteWholeDirectory(dir);
-        }
-    }
-
-    private static void CopyFiles(string source, string replica)
-    {
-        var sourceFiles = Directory.GetFiles(source, "*", SearchOption.AllDirectories);
-
-        foreach (var file in sourceFiles)
-        {
-            var replicaFile = file.Replace(source, replica);
-            Util.TryCopyFile(file, replicaFile);
-        }
-    }
-
-    private static void DeleteFiles(string source, string replica)
-    {
-        var replicaFiles = Directory.GetFiles(replica, "*", SearchOption.AllDirectories);
-
-        foreach (var file in replicaFiles)
-        {
-            var sourceFile = file.Replace(replica, source);
-
-            if (File.Exists(sourceFile)) continue; //Source file exists and the content is the same, so we don't need to delete the replica
-
-            Util.TryDeleteFile(file);
-        }
+        Console.WriteLine("Press any key to stop...");
+        Console.ReadKey();
     }
 }
