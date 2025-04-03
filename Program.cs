9a26fb6035713a0c36cb9ff1ecaed1b25b0fa1c7
Lubos Dohnal <e-lubos.dohnal@embedit.com>
2025-04-03 11:57:28 +0200

    Verify folders

--- Program.cs
+++ Program.cs
@@ -4,10 +4,16 @@ class Program
 {
     private static string _sourcePath = string.Empty;
     private static string _replicaPath = string.Empty;
-    private static string _logFilePath = string.Empty;
+    private static string _logPath = string.Empty;
     private static int _syncInterval = 0;
 
     public static void Main(string[] args)
+    {
+        VerifyArguments(args);
+        VerifyFolders();
+    }
+
+    private static void VerifyArguments(string[] args)
     {
         if (args.Length != 4)
         {
@@ -22,6 +28,35 @@ class Program
             Console.WriteLine("Invalid interval. Must be a positive integer value (in seconds).");
             return;
         }
-        _logFilePath = args[3];
+        _logPath = args[3];
+    }
+
+    private static void VerifyFolders()
+    {
+        if (!Directory.Exists(_sourcePath))
+        {
+            Console.WriteLine($"Source path '{_sourcePath}' does not exist.");
+            return;
+        }
+
+        try
+        {
+            Directory.CreateDirectory(_replicaPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating replica folder '{_replicaPath}': {ex.Message}");
+            return;
+        }
+
+        try
+        {
+            Directory.CreateDirectory(_logPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating log folder '{_logPath}': {ex.Message}");
+            return;
+        }
     }
 }
